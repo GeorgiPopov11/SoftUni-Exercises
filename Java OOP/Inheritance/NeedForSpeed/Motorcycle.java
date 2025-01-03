d73ba9571e0bcf0d6a53bd0c73fe679522adc9f5
GeorgiPopov11 <georgipopov11@gmail.com>
2025-01-03 13:26:14 +0200

    Update Motorcycle.java

--- Java OOP/Inheritance/NeedForSpeed/Motorcycle.java
+++ Java OOP/Inheritance/NeedForSpeed/Motorcycle.java
@@ -3,8 +3,12 @@ package Inheritance.NeedForSpeed;
 public class Motorcycle extends Vehicle{
 
     //git desktop testing
+    //git desktop testing 1
 
     public Motorcycle(double fuel, int horsePower) {
+
         super(fuel, horsePower);
+
+
     }
 }
