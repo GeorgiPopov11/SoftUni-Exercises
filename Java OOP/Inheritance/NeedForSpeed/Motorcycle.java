9f348100e129bc32bcb7b07a4b110d23842d5fef
GeorgiPopov11 <georgipopov11@gmail.com>
2025-01-03 13:14:24 +0200

    gitdekstop testing

--- Java OOP/Inheritance/NeedForSpeed/Motorcycle.java
+++ Java OOP/Inheritance/NeedForSpeed/Motorcycle.java
@@ -2,6 +2,8 @@ package Inheritance.NeedForSpeed;
 
 public class Motorcycle extends Vehicle{
 
+    //git desktop testing
+
     public Motorcycle(double fuel, int horsePower) {
         super(fuel, horsePower);
     }
